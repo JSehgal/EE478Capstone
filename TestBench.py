d362be028f25a091c4d68cca0330946207990fad
Jillian Sehgal <jillian@sehgal.net>
2014-11-20 11:33:26 -0800

    Depth code added
    Test images moved to documentation.
    Depth test images added.
    ExtarctDepth.py added. Contains code for finding epilines and extraction
    depth. Not yet tested.
    Testbench.py now has correct COM port for Jillian's computer.

--- TestBench.py
+++ TestBench.py
@@ -1,6 +1,6 @@
 import serial, time
-beagle = serial.Serial('COM1', 115200, timeout=.1)
-time.sleep(1)
+beagle = serial.Serial('COM5', 115200, timeout=.1)
+time.sleep(5)
 beagle.write("Testbench connected")
 while True:
 	data = beagle.readline()
