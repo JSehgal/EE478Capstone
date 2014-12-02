965a0704dc9cc5edeb26655b4cbc6fb58b438a59
nadiaak <nadiak3@uw.edu>
2014-12-02 12:48:08 -0800

    adc files added

--- cam/camimg.c
+++ cam/camimg.c
@@ -30,7 +30,7 @@
 * Local Macro Declarations                                                    *
 ******************************************************************************/
 
-#define PRU_NUM 	 1
+#define PRU_NUM 	 0
 #define ADDEND1	 	 0x98765400u
 #define ADDEND2		 0x12345678u
 #define ADDEND3		 0x10210210u
