0b1f54faf70883b30d06248a504a5dfc05e107b8
Jillian Sehgal <jillian@sehgal.net>
2014-12-07 20:42:44 -0800

    Updated for final version

--- TestBench.py
+++ TestBench.py
@@ -4,34 +4,29 @@ from multiprocessing import Process
 
 def getCommands():
 	while True:
-		command = raw_input("Enter start. stop or reset command:")
-		print command, 'command received' 
-
-def receiveData():
-	while True:
+		command = raw_input("Enter start. stop or reset command: ")
+		
 		# If command has been given, execute it and then clear it
 		if(command == 'start'):
+			print command, 'command received' 
 			beagle.write('s')
 			command = 'none'
 		elif(command == 'stop'):
+			print command, 'command received' 
 			beagle.write('p')
 			command = 'none'
 		elif(command == 'reset'):
+			print command, 'command received' 
 			beagle.write('r')
 			command = 'none'
-
-		# Receive data from beaglebone
-		data = beagle.readline()
-		if data:
-			print data
+		else:
+			print 'command not valid'
 
 # Setup serial connection
-beagle = serial.Serial('COM5', 9600, timeout=.1)
-time.sleep(5)
+beagle = serial.Serial('/dev/ttyUSB0', 9600, timeout=.1)
+time.sleep(1)
 
 beagle.write("Testbench connected")
 command = 'none'
 
-receiveData()
-
-
+getCommands()
